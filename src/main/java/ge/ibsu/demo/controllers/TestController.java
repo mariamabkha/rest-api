de3ce51b78da9b99c4a62cdb28a9ac82b03630a4
mariamabkha <20100238@ibsu.edu.ge>
2023-03-23 21:18:35 +0400

    java files

---
+++ src/main/java/ge/ibsu/demo/controllers/TestController.java
@@ -0,0 +1,24 @@
+package ge.ibsu.demo.controllers;
+
+import ge.ibsu.demo.dto.Test;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/test")
+public class TestController {
+
+    @RequestMapping(value="/call", method = RequestMethod.GET, produces = {"application/json"})
+     public Test test(){
+         return new Test("mariam", "abkhazava");
+     }
+
+     @RequestMapping(value="/post", method = RequestMethod.POST, produces = {"application/json"})
+     public Test testPost(@RequestBody Test test){
+         System.out.println(test.getFirstName() + " " + test.getLastName());
+         return new Test(test.getFirstName() + " from server", test.getLastName());
+     }
+
+}
